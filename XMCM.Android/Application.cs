4a98c555453b5bc602725bd149e0dc1c24b5b2c6
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2020-01-16 20:55:06 +0800

    Set up Xamarin.Android Project

---
+++ XMCM.Android/Application.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Runtime;
+
+using Caliburn.Micro;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using XMCM.ViewModels;
+
+namespace XMCM.Droid
+{
+	[Application]
+	public class Application : CaliburnApplication
+	{
+		//* Private Properties
+		private SimpleContainer _container;
+
+		//* Constructors
+		public Application(IntPtr javaReference, JniHandleOwnership transfer)
+			  : base(javaReference, transfer) { }
+
+		//* Overridden Methods
+		protected override void BuildUp(object instance) =>
+			_container.BuildUp(instance);
+
+		protected override void Configure()
+		{
+			_container = new SimpleContainer();
+
+			_container
+				.Instance(_container);
+
+			_container
+				.Singleton<App>();
+
+			// TODO: Register any Platform Specific abstractions here
+		}
+
+		protected override IEnumerable<object> GetAllInstances(Type service) =>
+			_container.GetAllInstances(service);
+
+		protected override object GetInstance(Type service, string key) =>
+			_container.GetInstance(service, key);
+
+		public override void OnCreate()
+		{
+			base.OnCreate();
+
+			Initialize();
+		}
+
+		protected override IEnumerable<Assembly> SelectAssemblies()
+		{
+			// Get a list of all assemblies that will be used by the
+			// IoC container
+			return new[]
+			{
+				GetType().Assembly,
+				typeof(ShellViewModel).Assembly
+			};
+		}
+	}
+}
--- XMCM.Android/MainActivity.cs
+++ XMCM.Android/MainActivity.cs
@@ -1,16 +1,21 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
+
+using Caliburn.Micro;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace XMCM.Droid
 {
-    [Activity(Label = "XMCM", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "XMCM",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -20,10 +25,11 @@ namespace XMCM.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+            Forms.Init(this, savedInstanceState);
+            LoadApplication(IoC.Get<App>());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions,
+            [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
